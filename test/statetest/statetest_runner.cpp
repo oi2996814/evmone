77100bb1edb1498be252f6dd00bceb9b6560b3cf
Paweł Bylica <pawel@hepcolgum.band>
2025-12-09 20:28:48 +0100

    test: In state test do block post-processing only for valid transaction (#1395)
    Modify the state test execution logic so that the block post-processing
    by `finalize()` is done only if the transaction in the test is valid.
    This goes by the logic that if the transaction is invalid the "virtual"
    block is invalid too and the state should remain unchanged.
    
    This issue has been undetected because the `finalize()` only creates
    empty coinbase account which is later filtered out in revisions from
    Spurious Dragon.

--- test/statetest/statetest_runner.cpp
+++ test/statetest/statetest_runner.cpp
@@ -28,11 +28,15 @@ void run_state_test(const StateTransitionTest& test, evmc::VM& vm, bool trace_su
             auto state = test.pre_state;
             const auto blob_params = get_blob_params(rev, test.blob_schedule);
 
-            const auto res = test::transition(state, block, test.block_hashes, tx, rev, vm,
+            const auto res = transition(state, block, test.block_hashes, tx, rev, vm,
                 block.gas_limit, static_cast<int64_t>(state::max_blob_gas_per_block(blob_params)));
 
-            // Finalize block with reward 0.
-            test::finalize(state, rev, block.coinbase, 0, {}, {});
+            if (holds_alternative<state::TransactionReceipt>(res))
+            {
+                // If the transaction is valid, follow the state test convention and do minimal
+                // block post-processing with the block reward of 0.
+                finalize(state, rev, block.coinbase, 0, {}, {});
+            }
 
             const auto state_root = state::mpt_hash(state);
 
